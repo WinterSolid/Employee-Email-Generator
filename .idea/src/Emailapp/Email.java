85f0d4f92db22b45485602fa9f9e20886c3a9aea
Wintersolid <ztanksley@luc.edu>
2020-07-21 17:29:57 -0500

    constructors build for Department- Fix *results == Expected

--- .idea/src/Emailapp/Email.java
+++ .idea/src/Emailapp/Email.java
@@ -1,5 +1,6 @@
 package Emailapp;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Email {
@@ -12,30 +13,29 @@ public class Email {
  private String alteremail;
 
  //Constructor for First name. Constructor for last name.
- public Email(String firstname,String lastname){
-  this.firstname = firstname;
-  this.lastname = lastname;
- System.out.println("Email created for: "+ firstname+" "+lastname);
+ public Email(String firstname, String lastname) {
+   this.firstname = firstname;
+   this.lastname = lastname;
+   System.out.println("Email created for: " + firstname + " " + lastname);
+
+
+ //constructor aking for Department setting private String setdepartment();
+
+    this.department = setdepartment();
+    System.out.println("Department Affiliation:" + this.department);
  }
-//constructor aking for Department
- private String setdepartment();
- System.out.print("What is the employees department?\nEnter # for Department:\n0 for no specific department\n1 for Development\n2 for Sales\n3 for HR\n4 for Public Safety");
-  Scanner in = new Scanner(System.in);
-  int Departchoice = in.nextInt();
+ private String setdepartment(){
+    System.out.print("What is the employees department?\nEnter # for Department:\n0 for no specific department\n1 for Development\n2 for Sales\n3 for HR\n4 for Public Safety");
+    Scanner in = new Scanner(System.in);
 
-  this.department = setdepartment();
+    int Departchoice = in.nextInt();
 
-
-   if (Departchoice == 1){return "(Development)";
+    if (Departchoice == 1){return "(Development)"; }
+    else if (Departchoice == 2){ return "(Sales)"; }
+    else if (Departchoice == 3){return "(HR)"; }
+    else if (Departchoice == 4){return "(Public Safety)"; }
+    else{ return "(None)"; }
    }
-   else if (Departchoice == 2){ return "(Sales)";
-   }
-   else if (Departchoice == 3){return "(HR)";
-   }
-   else if (Departchoice == 4){return "(Public Safety)";
-   }
-   else (Departchoice == 0){ return "(No specific)";
- }
 
 
  // Constructor for random password.
