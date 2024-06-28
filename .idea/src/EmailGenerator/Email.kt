226cd35396d18b635451d80b1d21df63643055f4
wintersolid <ztanksley@luc.edu>
2024-06-28 14:01:12 -0700

    Partial Refactor of Email.java to Email.kt, Rename getters/steers to be more specific. Add EmailController.kt to project

---
+++ .idea/src/EmailGenerator/Email.kt
@@ -0,0 +1,17 @@
+package EmailGenerator
+
+class Email {
+}
+package EmailGenerator
+
+import EmailGenerator.controller
+import EmailGenerator.view
+import EmailGenerator.model
+
+fun main() {
+    val model = Email("", "")
+    val view = EmailView()
+    val controller = EmailController(model, view)
+
+    controller.createEmailAccount()
+}
---
+++ .idea/src/EmailGenerator/model/Email.kt
@@ -0,0 +1,49 @@
+package EmailGenerator.model
+
+
+const val companySuffix: String = "starkindustries.com"
+
+class Email (var firstname: String?, var lastname: String?){
+
+    var password: String? = null
+    var passwordlength: Int = 16
+    var department: String? = null
+    var email: String? = null
+    var alteremail: String? = null
+    var mailboxcap: Int = 0
+
+    init {
+        // Initialize email and password in the constructor
+        setEmployeeDepartment("")
+        generateEmployeePassword()
+    }
+
+    fun setEmployeeDepartment(department: String) {
+        this.department = department
+        this.email = "${firstname.toLowerCase()}.${lastname.toLowerCase()}@${department.toLowerCase()}.$companySuffix"
+    }
+
+    fun generateEmployeePassword() {
+        this.password = createRandomPassword(passwordLength)
+    }
+
+    private fun createRandomPassword(length: Int): String {
+        val passwordSet = "abcdefghijklmnopqrstuvwxyz1234567890)(*&^%$#@!"
+        val password = CharArray(length)
+        for (i in password.indices) {
+            val rand = (Math.random() * passwordSet.length).toInt()
+            password[i] = passwordSet[rand]
+        }
+        return String(password)
+    }
+
+    fun showInfo(): String {
+        return """
+            Name: ${firstname.toUpperCase()} ${lastname.toUpperCase()}
+            Email: $email
+            Password: $password
+            Mailbox Capacity: $mailboxCap
+        """.trimIndent()
+    }
+}
+}
